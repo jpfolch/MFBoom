022caa5295d42512534828856ab5a57ee3a68427
Jose Pablo Folch <jpfolch@outlook.com>
2022-03-09 11:08:05 +0000

    defines mf batch environment

--- environment.py
+++ environment.py
@@ -13,6 +13,7 @@ class mfBatchEnvironment():
         # add function
         self.func = func
         self.dim = func.dim
+        self.num_of_fidelities = func.num_of_fidelities
         self.initialize_env()
 
     def initialize_env(self):
@@ -27,22 +28,28 @@ class mfBatchEnvironment():
         # add new queries and fidelities to list
         self.query_list = np.concatenate((self.query_list, new_queries))
         self.fidelities_list = np.concatenate((self.fidelities_list, fidelities))
-        # calculate evaluation times and subtract one
+        # calculate evaluation times and join them, subtract one
         new_eval_times = self.func.eval_times(fidelities)
         self.query_times = np.concatenate((self.query_times, new_eval_times)) - 1
         # check if we have queries to return
-        queries_finished = (self.query_times == 0)
+        queries_finished = (self.query_times == 0).reshape(-1)
         # choose finished queries from query list
         queries_out = self.query_list[queries_finished, :]
         fidelities_out = self.fidelities_list[queries_finished, :]
         # initialize observation list
         obs = []
-        for query, fidelity in zip(queries_out, fidelities):
+        for query, fidelity in zip(queries_out, fidelities_out):
+            query = query.reshape(1, -1)
+            fidelity = int(fidelity)
             obs_out = self.func.evaluate(query, fidelity)
             obs.append(obs_out)
         
         if len(obs) > 0:
             obs = np.array(obs).reshape(-1, 1)
+            if queries_out.shape[0] > len(obs):
+                print('wut')
+        else:
+            obs = None
 
         # redefine the queries being evaluated
         self.query_list = self.query_list[np.invert(queries_finished), :]
@@ -58,13 +65,11 @@ class mfBatchEnvironment():
         number_of_queries_left = self.query_list.shape[0]
         obs = []
         for i in range(number_of_queries_left):
-            query = self.query_list[i, :]
-            fid = self.fidelities_list[i, :]
+            query = self.query_list[i, :].reshape(1, -1)
+            fid = self.fidelities_list[i, :].reshape(1, 1)
             obs.append(self.func.evaluate(query, fid))
         
         if len(obs) > 0:
             obs = np.array(obs).reshape(-1, 1)
         
         return self.query_list, self.fidelities_list, obs
-
-print('hola')
